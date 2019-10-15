37a35168b9e18771ebd04815d1531e9a5c8f2b70
Savya Saachi <savyasaachi.verma@gmail.com>
2019-10-15 12:55:35 -0500

    changed the resnet_tinyimagenet.py for testing

--- resnet_tinyimagenet.py
+++ resnet_tinyimagenet.py
@@ -42,6 +42,7 @@ def create_val_folder(val_dir):
         newpath = (os.path.join(path, folder))
     if not os.path.exists(newpath):  # check if folder exists
         os.makedirs(newpath)
+        print(newpath)
     # Check if image exists in default directory
     if os.path.exists(os.path.join(path, img)):
         os.rename(os.path.join(path, img), os.path.join(newpath, img))
@@ -84,9 +85,9 @@ def main():
 
     if 'val_' in os.listdir(val_dir+'images/')[0]:
         create_val_folder(val_dir)
-        val_dir = val_dir+'images/'
+        val_dir = val_dir + 'images/'
     else:
-        val_dir = val_dir+'images/'
+        val_dir = val_dir + 'images/'
     #train_ds = torchvision.datasets.ImageNet(train_dir, split='train', download=False, transform=augment_train_ds)
     # train_ds_loader = data.DataLoader(train_ds, batch_size=batch_size_train, shuffle=True, num_workers=8)
     # test_ds = torchvision.datasets.ImageNet(val_dir, split='val', download=False, transform=augment_test_ds)
@@ -116,124 +117,124 @@ def main():
         res_net = torch.nn.DataParallel(res_net)
         cudnn.benchmark = True
 
-    """___________ Training ___________"""
-
-    print("Starting Training")
-
-    """Criterion Function: Softmax + Log-Likelihood"""
-    loss_fn = nn.CrossEntropyLoss()
-    """Adam Optimizer (as it takes advantage of both RMSDrop and Momentum"""
-    optimizer = optim.Adam(res_net.parameters(), lr=learning_rate)
-
-    test_acc_list = []
-    epochs_list = [x for x in range(epochs)]
-
-    for epoch in range(start_epoch, epochs):
-
-        cur_loss = 0.0
-        total_correct = 0
-        total_samples = 0
-
-        """ Overflow error in the optimizer if the step size is not reset."""
-        if epoch > 8:
-            for group in optimizer.param_groups:
-                for p in group['params']:
-                    state = optimizer.state[p]
-                    if state['step'] >= 1024:
-                        state['step'] = 1000
-
-        for i, (inputs, labels) in enumerate(train_ds_loader):
-
-            """Transfer inputs and labels to CUDA if available"""
-            inputs = Variable(inputs).to(device)
-            labels = Variable(labels).to(device)
-
-            """Loss function requires the inputs to be wrapped in variables"""
-            # inputs = Variable(inputs)
-
-            """Torch tends to take cumulative gradients which is not required so setting it to zero after each batch"""
-            optimizer.zero_grad()
-
-            outputs = res_net(inputs)
-            loss = loss_fn(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            cur_loss += loss.item()
-            avg_loss = cur_loss / (i + 1)
-
-            _, predicted_label = torch.max(outputs, 1)
-            # print(predicted_label.shape, labels.shape)
-            total_samples += labels.shape[0]
-            # arr = (predicted_label == labels).numpy()
-            # print(np.sum(arr))
-            """can not use numpy as the tensors are in CUDA"""
-            total_correct += predicted_label.eq(labels.long()).float().sum().item()
-            accuracy = total_correct / total_samples
-
-            if i % 100 == 0:
-                print('Training [epoch: %d, batch: %d] loss: %.3f, accuracy: %.5f' %
-                      (epoch + 1, i + 1, avg_loss, accuracy))
-
-        test_acc_list.append(test(device, loss_fn, res_net, test_ds_loader))
-
-        """Saving model after every 5 epochs"""
-        if (epoch + 1) % 5 == 0:
-            print('==> Saving model ...')
-            state = {
-                'res_net_model': res_net.state_dict(),
-                'epoch': epoch,
-            }
-            if not os.path.isdir('./Checkpoint'):
-                os.mkdir('Checkpoint')
-            torch.save(state, './Checkpoint/model_state.pt')
-
-    print("Training Completed!")
-
-    """___________ Testing ____________"""
-    print("Testing Started")
-    """Puts model in testing state"""
-    res_net.eval()
-
-    accuracy = test(device, loss_fn, res_net, test_ds_loader)
-
-    print("Testing Completed with accuracy:" + str(accuracy))
-
-    with open('graph_resnet_tinyimagenet.csv', 'w') as result_file:
-        wr = csv.writer(result_file, dialect='excel')
-        wr.writerow(test_acc_list)
-
-    print("Saved Test Accuracy list for graph")
-
-
-def test(device, loss_fn, res_net, test_ds_loader):
-    cur_loss = 0.0
-    total_correct = 0
-    total_samples = 0
-    """Do testing under the no_grad() context so that torch does not store/use these actions to calculate gradients"""
-    with torch.no_grad():
-        for i, (inputs, labels) in enumerate(test_ds_loader):
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-
-            inputs = Variable(inputs)
-
-            outputs = res_net(inputs)
-            loss = loss_fn(outputs, labels)
-
-            cur_loss += loss.item()
-            avg_loss = cur_loss / (i + 1)
-
-            _, predicted_label = torch.max(outputs, 1)
-            total_samples += labels.shape[0]
-            # arr = (predicted_label == labels).numpy()
-            total_correct += predicted_label.eq(labels.long()).float().sum().item()
-            accuracy = total_correct / total_samples
-
-            if i % 50 == 0:
-                print('Testing [batch: %d] loss: %.3f, accuracy: %.5f' %
-                      (i + 1, avg_loss, accuracy))
-    return accuracy
+#     """___________ Training ___________"""
+#
+#     print("Starting Training")
+#
+#     """Criterion Function: Softmax + Log-Likelihood"""
+#     loss_fn = nn.CrossEntropyLoss()
+#     """Adam Optimizer (as it takes advantage of both RMSDrop and Momentum"""
+#     optimizer = optim.Adam(res_net.parameters(), lr=learning_rate)
+#
+#     test_acc_list = []
+#     epochs_list = [x for x in range(epochs)]
+#
+#     for epoch in range(start_epoch, epochs):
+#
+#         cur_loss = 0.0
+#         total_correct = 0
+#         total_samples = 0
+#
+#         """ Overflow error in the optimizer if the step size is not reset."""
+#         if epoch > 8:
+#             for group in optimizer.param_groups:
+#                 for p in group['params']:
+#                     state = optimizer.state[p]
+#                     if state['step'] >= 1024:
+#                         state['step'] = 1000
+#
+#         for i, (inputs, labels) in enumerate(train_ds_loader):
+#
+#             """Transfer inputs and labels to CUDA if available"""
+#             inputs = Variable(inputs).to(device)
+#             labels = Variable(labels).to(device)
+#
+#             """Loss function requires the inputs to be wrapped in variables"""
+#             # inputs = Variable(inputs)
+#
+#             """Torch tends to take cumulative gradients which is not required so setting it to zero after each batch"""
+#             optimizer.zero_grad()
+#
+#             outputs = res_net(inputs)
+#             loss = loss_fn(outputs, labels)
+#             loss.backward()
+#             optimizer.step()
+#
+#             cur_loss += loss.item()
+#             avg_loss = cur_loss / (i + 1)
+#
+#             _, predicted_label = torch.max(outputs, 1)
+#             # print(predicted_label.shape, labels.shape)
+#             total_samples += labels.shape[0]
+#             # arr = (predicted_label == labels).numpy()
+#             # print(np.sum(arr))
+#             """can not use numpy as the tensors are in CUDA"""
+#             total_correct += predicted_label.eq(labels.long()).float().sum().item()
+#             accuracy = total_correct / total_samples
+#
+#             if i % 100 == 0:
+#                 print('Training [epoch: %d, batch: %d] loss: %.3f, accuracy: %.5f' %
+#                       (epoch + 1, i + 1, avg_loss, accuracy))
+#
+#         test_acc_list.append(test(device, loss_fn, res_net, test_ds_loader))
+#
+#         """Saving model after every 5 epochs"""
+#         if (epoch + 1) % 5 == 0:
+#             print('==> Saving model ...')
+#             state = {
+#                 'res_net_model': res_net.state_dict(),
+#                 'epoch': epoch,
+#             }
+#             if not os.path.isdir('./Checkpoint'):
+#                 os.mkdir('Checkpoint')
+#             torch.save(state, './Checkpoint/model_state.pt')
+#
+#     print("Training Completed!")
+#
+#     """___________ Testing ____________"""
+#     print("Testing Started")
+#     """Puts model in testing state"""
+#     res_net.eval()
+#
+#     accuracy = test(device, loss_fn, res_net, test_ds_loader)
+#
+#     print("Testing Completed with accuracy:" + str(accuracy))
+#
+#     with open('graph_resnet_tinyimagenet.csv', 'w') as result_file:
+#         wr = csv.writer(result_file, dialect='excel')
+#         wr.writerow(test_acc_list)
+#
+#     print("Saved Test Accuracy list for graph")
+#
+#
+# def test(device, loss_fn, res_net, test_ds_loader):
+#     cur_loss = 0.0
+#     total_correct = 0
+#     total_samples = 0
+#     """Do testing under the no_grad() context so that torch does not store/use these actions to calculate gradients"""
+#     with torch.no_grad():
+#         for i, (inputs, labels) in enumerate(test_ds_loader):
+#             inputs = inputs.to(device)
+#             labels = labels.to(device)
+#
+#             inputs = Variable(inputs)
+#
+#             outputs = res_net(inputs)
+#             loss = loss_fn(outputs, labels)
+#
+#             cur_loss += loss.item()
+#             avg_loss = cur_loss / (i + 1)
+#
+#             _, predicted_label = torch.max(outputs, 1)
+#             total_samples += labels.shape[0]
+#             # arr = (predicted_label == labels).numpy()
+#             total_correct += predicted_label.eq(labels.long()).float().sum().item()
+#             accuracy = total_correct / total_samples
+#
+#             if i % 50 == 0:
+#                 print('Testing [batch: %d] loss: %.3f, accuracy: %.5f' %
+#                       (i + 1, avg_loss, accuracy))
+#     return accuracy
 
 
 if __name__ == "__main__":
